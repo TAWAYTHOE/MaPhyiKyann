e0049635bdfff57c1a501f87c6d89f3a740cc634
AungMyo <47168280+TAWAYTHOE@users.noreply.github.com>
2020-02-17 10:16:09 +0630

    updated
    use this

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000  ;
 
 const ViberBot = require('viber-bot').Bot;
 const BotEvents = require('viber-bot').Events
+const TextMessage = require('viber-bot').Message.Text;
+const UrlMessage = require('viber-bot').Message.Url;
+const ContactMessage = require('viber-bot').Message.Contact;
+const PictureMessage = require('viber-bot').Message.Picture;
+const VideoMessage = require('viber-bot').Message.Video;
+const LocationMessage = require('viber-bot').Message.Location;
+const StickerMessage = require('viber-bot').Message.Sticker;
+const RichMediaMessage = require('viber-bot').Message.RichMedia;
+const KeyboardMessage = require('viber-bot').Message.Keyboard;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => {res.send('Hello World!')} )
 
 //start_here
 const bot = new ViberBot({
@@ -16,8 +23,12 @@ const bot = new ViberBot({
 	avatar: "http://viber.com/avatar.jpg" // It is recommended to be 720x720, and no more than 100kb.
 });
 
+app.use("/viber", bot.middleware())
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
 // Perfect! Now here's the key part:
 bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
 	// Echo's back the message to the client. Your bot logic should sit here.
 	response.send(message);
 });
+
